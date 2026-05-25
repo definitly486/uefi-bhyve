cf3766a419272a500142f9ba231c3b423b1ccec9
Your Name <you@example.com>
2026-05-25 23:13:26 +0300

    новый файл:    win8/note 	новый файл:    win8/setup.bat 	новый файл:    win8/win8_iso_copy.sh

---
+++ win8/win8_iso_copy.sh
@@ -0,0 +1,2 @@
+mkdir -p ~/win8_iso_copy
+7z x /ntfs-2TB/downloads/windows_iso/9600.16384.WINBLUE_RTM.130821-1623_X64FRE_EMBEDDEDINDUSTRY_RU-RU-IRM_CEI_X64FRE_RU-RU_DV5.ISO -o/home/definitly/win8_iso_copy
