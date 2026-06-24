1b01b94689d10f75e47f5879fb58e37c6ce182ee
Your Name <you@example.com>
2026-06-25 01:10:42 +0300

    изменено:      win10/note 	изменено:      win10/setup.bat 	изменено:      win10/win10_iso_copy.sh 	новый файл:    win10/winpeshl.ini

--- win10/win10_iso_copy.sh
+++ win10/win10_iso_copy.sh
@@ -1,2 +1,2 @@
 mkdir -p ~/win10_iso_copy
-7z x /ntfs-2TB/vm/ISO/Win10_22H2_19045_5965_Russian_x64.iso -o/home/definitly/win10_iso_copy
+7z x /ntfs-2TB/vm/ISO/ru-ru_windows_10_enterprise_ltsc_2021_x64_dvd.iso -o/home/definitly/win10_iso_copy
