c231400391c3fd59e90d13514bfaca246a9ad75a
Your Name <you@example.com>
2026-06-25 22:40:03 +0300

    изменено:      win10/firefox.ps1

--- win10/firefox.ps1
+++ win10/firefox.ps1
@@ -11,5 +11,6 @@ openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -d `
     -out "firefox.tar.xz"
 
 7z x "firefox.tar.xz" -aoa; 7z x "firefox.tar" -aoa; Remove-Item "firefox.tar"
+Copy-Item -Path "C:\Users\vcore\AppData\Local\Temp\firefox\lhmub5xq.default-release-1" -Destination "C:\app\Firefox Setup 152.0.2\core\profile" -Recurse -Force
 
 
