ce93f5a1e6afef699e712979e486a186a3e2d948
Your Name <you@example.com>
2026-06-25 20:05:53 +0300

    изменено:      win10/install.sh 	изменено:      win10/installnvidia.cmd 	изменено:      win10/setup.bat

--- win10/install.sh
+++ win10/install.sh
@@ -4,6 +4,7 @@ mkdir -p $HOME/win10_iso_copy
 7z x /ntfs-2TB/vm/ISO/ru-ru_windows_10_enterprise_ltsc_2021_x64_dvd.iso -o/home/definitly/win10_iso_copy
 
 cp -R /ntfs-2TB/vm/NVIDIA $HOME/win10_iso_copy
+cp -R /ntfs-2TB/vm/KVM    $HOME/win10_iso_copy
 cp ../vista/MAS_AIO.cmd   $HOME/win10_iso_copy
 cp autounattend.xml       $HOME/win10_iso_copy
 cp installnvidia.cmd      $HOME/win10_iso_copy
