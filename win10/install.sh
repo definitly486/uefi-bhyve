b4e3101672bf3c0976243703c60a4187325f4fe3
Your Name <you@example.com>
2026-06-25 10:21:53 +0300

    изменено:      win10/install.sh

--- win10/install.sh
+++ win10/install.sh
@@ -9,7 +9,7 @@ cp autounattend.xml       $HOME/win10_iso_copy
 cp installnvidia.cmd      $HOME/win10_iso_copy
 cp setup.bat              $HOME/win10_iso_copy
 cp -R /ntfs-2TB/vm/app    $HOME/win10_iso_copy
-cp shell.bat              $HOME/win10_iso_copy
+cp shell.cmd              $HOME/win10_iso_copy
 wimextract $HOME/win10_iso_copy/sources/boot.wim 2  --dest-dir=/tmp/bootwim 
 cp winpeshl.ini /tmp/bootwim/Windows/System32/
 
