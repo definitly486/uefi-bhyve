4a51ec26bc3f643bf435cc1c9e65510d4d19e841
Your Name <you@example.com>
2026-06-25 08:07:21 +0300

    изменено:      win10/autounattend.xml 	изменено:      win10/installnvidia.cmd

--- win10/installnvidia.cmd
+++ win10/installnvidia.cmd
@@ -49,5 +49,23 @@ echo Done.
 echo If GPU is not installed yet, drivers will activate automatically after detection.
 echo.
 
+
+:: 1. Включаем встроенную учетную запись Гостя
+net user Гость /active:yes
+
+:: 2. Разрешаем использовать пустые пароли для сетевых подключений
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v "LimitBlankPasswordUse" /t REG_DWORD /d 0 /f
+
+:: 3. Разрешаем небезопасный гостевой вход в SMB (критично для Windows 10/11)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters" /v "AllowInsecureGuestAuth" /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /v "RestrictNullSessAccess" /t REG_DWORD /d 0 /f
+
+:: 4. Включаем общий доступ без парольной защиты
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v "everyoneincludesanonymous" /t REG_DWORD /d 1 /f
+
+:: 5. Перезапускаем сетевую службу для применения настроек
+net stop LanmanServer /y
+net start LanmanServer
+
 pause
 endlocal
