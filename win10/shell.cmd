1dbbf5b02120f4e3946e9b097857f3ac2dffe10f
Your Name <you@example.com>
2026-06-25 10:58:57 +0300

    изменено:      win10/shell.cmd

--- win10/shell.cmd
+++ win10/shell.cmd
@@ -1,14 +1,25 @@
 @echo off
 chcp 65001 > nul
 
-echo [1/3] Разрешаем выполнение скриптов в PowerShell...
-powershell -NoProfile -Command "Set-ExecutionPolicy RemoteSigned -Scope LocalMachine -Force"
+echo [1/4] Разрешаем выполнение скриптов в PowerShell...
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
 
-echo [2/3] Проверяем и создаем профиль PowerShell, если его нет...
-powershell -NoProfile -Command "if (!(Test-Path $PROFILE)) { New-Item -Type File -Path $PROFILE -Force }"
+echo [2/4] Проверяем и создаем профиль PowerShell...
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+"if (!(Test-Path $PROFILE)) { New-Item -ItemType File -Path $PROFILE -Force | Out-Null }"
 
-echo [3/3] Добавляем функции git и bash в профиль...
-powershell -NoProfile -Command "$functions = 'function git { & ''C:\app\PortableGit-2.54.0-64-bit.7z\bin\git.exe'' @args }', 'function bash { & ''C:\app\PortableGit-2.54.0-64-bit.7z\bin\bash.exe'' @args }'; Add-Content -Path $PROFILE -Value $functions -Encoding utf8"
+echo [3/4] Добавляем функции git и bash в профиль...
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+"$content = @'
+function git {
+& 'C:\app\PortableGit-2.54.0-64-bit.7z\bin\git.exe' @args
+}
 
-echo [4/4] Запуск PowerShell и применение профиля...
-powershell -NoExit -Command ". $PROFILE"
+function bash {
+& 'C:\app\PortableGit-2.54.0-64-bit.7z\bin\bash.exe' @args
+}
+'@;
+Add-Content -Path $PROFILE -Value $content"
+
+echo [4/4] Запускаем PowerShell с обновленным профилем...
+powershell -NoExit -ExecutionPolicy Bypass -Command ". $PROFILE"
