686adaa255bdf2a65e83f57a73c546c7dce5098c
Your Name <you@example.com>
2026-06-26 12:12:18 +0300

    изменено:      win10/install.sh

--- win10/install.sh
+++ win10/install.sh
@@ -29,7 +29,7 @@ nice -n 15  cp -R /ntfs-2TB/vm/NVIDIA    $HOME/win10_iso_copy
 nice -n 15  cp -R /ntfs-2TB/vm/NetKVM    $HOME/win10_iso_copy
 cp ../vista/MAS_AIO.cmd      $HOME/win10_iso_copy
 cp autounattend.xml          $HOME/win10_iso_copy
-cp installnvidia.cmd         $HOME/win10_iso_copy
+cp install.cmd         $HOME/win10_iso_copy
 cp setup.bat                 $HOME/win10_iso_copy
 nice -n 15  cp -R /ntfs-2TB/vm/app       $HOME/win10_iso_copy
 cp shell.cmd                 $HOME/win10_iso_copy
