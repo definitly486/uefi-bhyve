b8d73bddbffa389ec166d3cae10d03b2487abc47
Your Name <you@example.com>
2026-06-30 08:04:58 +0300

    изменено:      win10/Microsoft.PowerShell_profile.ps1

--- win10/Microsoft.PowerShell_profile.ps1
+++ win10/Microsoft.PowerShell_profile.ps1
@@ -1,27 +1,7 @@
-function git {
-& 'C:\app\PortableGit-2.54.0-64-bit.7z\bin\git.exe' @args
-}
-
-function bash {
-& 'C:\app\PortableGit-2.54.0-64-bit.7z\bin\bash.exe' @args
-}
-
-function openssl {
-& 'C:\app\openssl-3.5.7\x64\bin\openssl.exe' @args
-}
-
-function 7z {
-& 'C:\app\7z2600-x64\7z.exe' @args
-}
-
-function mpv {
-& 'C:\app\mpv-x86_64-20260626-git-c75b8d2cca\mpv.exe' @args
-}
-
-function yt-dlp {
-& 'C:\app\yt-dlp\yt-dlp.exe' @args
-}
-
-function streamlink {
-& 'C:\app\streamlink-8.4.0-1-py314-x86_64\bin\streamlink.exe' @args
-}
+# Добавляем все ваши портативные программы в PATH текущей сессии
+$env:PATH += ";C:\app\PortableGit-2.54.0-64-bit.7z\bin"
+$env:PATH += ";C:\app\openssl-3.5.7\x64\bin"
+$env:PATH += ";C:\app\7z2600-x64"
+$env:PATH += ";C:\app\mpv-x86_64-20260626-git-c75b8d2cca"
+$env:PATH += ";C:\app\yt-dlp"
+$env:PATH += ";C:\app\streamlink-8.4.0-1-py314-x86_64\bin"
