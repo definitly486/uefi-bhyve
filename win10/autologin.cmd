f6a4fef3247746798cfbc6ae712caff88cfede28
Your Name <you@example.com>
2026-07-21 12:03:27 +0300

    изменено:      win10/autologin.cmd

--- win10/autologin.cmd
+++ win10/autologin.cmd
@@ -1,6 +1,15 @@
-::Автологин
+@echo off
 
+:: Проверка прав администратора
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d "vcore" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d "639639" /f
+
+echo Готово.
+pause
