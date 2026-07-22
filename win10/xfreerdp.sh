4d45a1406ed09ffd531a3a5fa70065af74601986
Your Name <you@example.com>
2026-07-22 10:43:30 +0300

    новый файл:    win10/xfreerdp.sh

---
+++ win10/xfreerdp.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#doas X :1 &
+#export DISPLAY=:1
+
+IP=$(
+    arp-scan --localnet 2>/dev/null |
+    awk '/NetApp/ { print $1; exit }'
+)
+
+if [ -z "$IP" ]; then
+    echo "NetApp не найден"
+    exit 1
+fi
+
+echo "NetApp: $IP"
+
+echo Y | xfreerdp /u:vcore  /p:639639 /w:1918 /h:1045  /v:$IP  /drive:home,/home/definitly  /sound
