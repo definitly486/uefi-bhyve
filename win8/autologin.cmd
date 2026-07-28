2a56efb5198ce90bae2cd351e2fa070803bb6571
Your Name <you@example.com>
2026-07-28 22:47:12 +0300

    новый файл:    win8/autologin.cmd 	новый файл:    win8/winpeshl.ini

---
+++ win8/autologin.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+:: Проверка прав администратора
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d "vcore" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d "639639" /f
+
+net use Z: \\192.168.8.101\Share /persistent:yes >nul 2>&1
+
+echo Готово.
+pause
